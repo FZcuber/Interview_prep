e51a5f47dc45528e812206ec331e46bf909f708f
FZcuber <62918647+FZcuber@users.noreply.github.com>
2022-07-06 13:23:23 -0400

    Added solution

--- Sliding window/ Best Time to Buy and Sell Stock.py
+++ Sliding window/ Best Time to Buy and Sell Stock.py
@@ -1,5 +1,18 @@
 def maxProfit(prices):
-    if len(prices) == 1:
+    if len(prices) < 2:
         return 0
 
-    max_profit = 0
+    result = 0
+
+    l = 0
+
+    for i in range(len(prices)):
+        if prices[i] < prices[l]:
+            l = i
+        else:
+            result = max(result, prices[i] - prices[l])
+
+    return result
+
+
+print(maxProfit(prices=[7, 1, 5, 3, 6, 4]))
